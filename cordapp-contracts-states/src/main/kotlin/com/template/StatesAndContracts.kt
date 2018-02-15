3c60ac6d917bbc117983f07bf690ec008c5bce0f
AlOK <alokvrm02@gmail.com>
2018-02-15 15:54:50 +0530

    First commit

---
+++ cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
@@ -0,0 +1,101 @@
+package com.template
+
+import net.corda.core.contracts.*
+import net.corda.core.crypto.NullKeys
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.AnonymousParty
+import net.corda.core.identity.Party
+import net.corda.core.transactions.LedgerTransaction
+import net.corda.core.transactions.TransactionBuilder
+
+// *****************
+// * Contract Code *
+// *****************
+// This is used to identify our contract when building a transaction
+val ALOK_CONTRACT_ID = "com.template.AlokContract"
+
+open class AlokContract : Contract {
+
+    interface Commands : CommandData{
+        class Issue :Commands
+        class Move :Commands
+    }
+    // A transaction is considered valid if the verify() function of the contract of each of the transaction's input
+    // and output states does not throw an exception.
+    override fun verify(tx: LedgerTransaction) {
+        // Verification logic goes here.
+
+        val groups =tx.groupStates(AlokState::withoutOwner)
+
+        val command = tx.commands.requireSingleCommand<AlokContract.Commands>()
+        val timeWindow : TimeWindow?=tx.timeWindow
+
+
+        for((inputs, outputs, _) in groups){
+            when(command.value){
+                is AlokContract.Commands.Move ->{
+                    val input = inputs.single()
+
+                    requireThat {
+                        "The transaction is signed by the owner of the Alok." using (input.owner.owningKey in command.signers)
+                        "The state is propagated." using (outputs.size == 1)
+                    }
+                }
+
+                is AlokContract.Commands.Issue->{
+                    val output = outputs.single()
+                    timeWindow?.untilTime ?: throw IllegalArgumentException("Instances must be timestamped !!")
+
+                    requireThat {
+                        "Output states are signed by command Signer." using (output.issuer.owningKey in command.signers)
+                        "Output contains a thought" using (!output.thought.equals(""))
+                        "Cant reissue an existing State." using inputs.isEmpty()
+                    }
+                }
+                else -> throw IllegalArgumentException("Command not found [E/1]!!")
+            }
+
+        }
+    }
+
+    companion object {
+        fun generateIssue(thought: String, issuer : AbstractParty, owner: AbstractParty, notary: Party): TransactionBuilder{
+            val state = AlokState(thought,issuer,owner)
+            val stateAndContract = StateAndContract(state, ALOK_CONTRACT_ID)
+            return TransactionBuilder(notary = notary).withItems(stateAndContract, Command(Commands.Issue(), issuer.owningKey))
+        }
+
+        fun generateMove(tx : TransactionBuilder,alok : StateAndRef<AlokState>, newOwner: AbstractParty){
+            tx.addInputState(alok)
+            val outputState = alok.state.data.withOwner(newOwner)
+            tx.addOutputState(outputState, ALOK_CONTRACT_ID)
+            tx.addCommand(Command(Commands.Move(),alok.state.data.owner.owningKey))
+
+        }
+    }
+
+    // Used to indicate the transaction's intent.
+//    interface Commands : CommandData {
+//        class Action : Commands
+//    }
+}
+
+// *********
+// * State *
+// *********
+data class TemplateState(val data: String) : ContractState {
+    override val participants: List<AbstractParty> get() = listOf()
+}
+
+// *********
+// * Custom State *
+// *********
+data class AlokState(val thought: String, val issuer : AbstractParty, val owner : AbstractParty): ContractState{
+    override val participants: List<AbstractParty> get() = listOf(owner,issuer)
+
+    fun withoutOwner() = copy(owner = AnonymousParty(NullKeys.NullPublicKey))
+
+    fun withOwner(newOwner : AbstractParty): AlokState{
+        return copy(owner=newOwner)
+    }
+}
---
+++ cordapp/src/main/kotlin/com/template/App.kt
@@ -0,0 +1,182 @@
+package com.template
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.TimeWindow
+import net.corda.core.contracts.requireThat
+import net.corda.core.flows.*
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.identity.Party
+import net.corda.core.messaging.CordaRPCOps
+import net.corda.core.serialization.SerializationWhitelist
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
+import net.corda.webserver.services.WebServerPluginRegistry
+import java.time.Duration
+import java.time.Instant
+import java.util.function.Function
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+
+
+val CORP_NAME = CordaX500Name(organisation = "BCS Learning", locality = "Sydney", country = "AU")
+internal val NOTARY_NAME = CordaX500Name(organisation = "Turicum Notary Service", locality = "Zurich", country = "CH", commonName="corda.notary.validating")
+internal val BOD_NAME = CordaX500Name(organisation = "Bank of Alok", locality = "Bloemfontein", country = "ZA")
+
+// *****************
+// * API Endpoints *
+// *****************
+@Path("template")
+class TemplateApi(val rpcOps: CordaRPCOps) {
+    // Accessible at /api/template/templateGetEndpoint.
+    @GET
+    @Path("templateGetEndpoint")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun templateGetEndpoint(): Response {
+        return Response.ok("Template GET endpoint.").build()
+    }
+}
+
+// *********
+// * Flows *
+// *********
+@InitiatingFlow
+@StartableByRPC
+class Initiator : FlowLogic<Unit>() {
+    @Suspendable
+    override fun call() {
+        return Unit
+    }
+}
+
+@InitiatedBy(Initiator::class)
+class Responder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
+    @Suspendable
+    override fun call() {
+        return Unit
+    }
+}
+
+@InitiatingFlow
+@StartableByRPC
+class AlokIssueRequest(val thought : String,val issuer : Party) : FlowLogic<SignedTransaction>(){
+    override val progressTracker = ProgressTracker()
+    @Suspendable
+    override fun call(): SignedTransaction {
+        val notary = serviceHub.networkMapCache.getNotary(NOTARY_NAME)?:throw FlowException("Notary not found !!")
+        val selfID = serviceHub.myInfo.legalIdentities[0]
+
+        val issueTxBuilder = AlokContract.generateIssue(thought,issuer,selfID, notary)
+
+        val bankSession = initiateFlow(issuer)
+
+        issueTxBuilder.setTimeWindow(TimeWindow.fromStartAndDuration(Instant.now(serviceHub.clock), Duration.ofMillis(10000)))
+
+        issueTxBuilder.verify(serviceHub)   // verifying transaction
+
+        val signedTx = serviceHub.signInitialTransaction(issueTxBuilder)    //signing transaction
+        val fullySignedTx = subFlow(CollectSignaturesFlow(signedTx, listOf(bankSession), CollectSignaturesFlow.tracker()))
+
+
+        return subFlow(FinalityFlow(fullySignedTx)) //Finalising the transaction
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
+
+@InitiatedBy(AlokIssueRequest::class)
+class AlokIssueResponse(val counterpartySession: FlowSession): FlowLogic<Unit>(){
+    @Suspendable
+    override fun call() {
+        val signTransactionFlow = object : SignTransactionFlow(counterpartySession, SignTransactionFlow.tracker()){
+            override fun checkTransaction(stx: SignedTransaction) = requireThat{
+                val output = stx.tx.outputs.single().data
+                "This must be Alok Transaction." using (output is AlokState)
+
+                val alok = output as AlokState
+                "The issuer of Alok must be the issuing node" using (alok.issuer.owningKey == ourIdentity.owningKey)
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        }
+
+        subFlow(signTransactionFlow)
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+
+
+@InitiatingFlow
+@StartableByRPC
+class AlokMoveRequest(val alok: StateAndRef<AlokState>, val newOwner : Party) : FlowLogic<SignedTransaction>(){
+    override val progressTracker = ProgressTracker()
+    @Suspendable
+    override fun call(): SignedTransaction {
+        val notary = serviceHub.networkMapCache.getNotary(NOTARY_NAME) ?: throw FlowException("Notary not found")
+
+        val txBuilder = TransactionBuilder(notary=notary)
+        AlokContract.generateMove(txBuilder, alok, newOwner)
+
+        val moveSession = initiateFlow(newOwner)
+
+        txBuilder.setTimeWindow(TimeWindow.fromStartAndDuration(Instant.now(serviceHub.clock), Duration.ofMillis(10000)))
+
+        txBuilder.verify(serviceHub)    // Verifying the transaction.
+
+        val signedTx = serviceHub.signInitialTransaction(txBuilder) // Signing the transaction.
+
+        val fullySignedTx = subFlow(CollectSignaturesFlow(signedTx, listOf(moveSession)))   // Obtaining the counterparty's signature.
+
+        return subFlow(FinalityFlow(fullySignedTx))      // Finalising the transaction.
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
+
+@InitiatedBy(AlokMoveRequest::class)
+class AlokMoveResponse(val counterpartySession: FlowSession) : FlowLogic<Unit>(){
+    @Suspendable
+    override fun call() {
+        val signTransactionFlow = object : SignTransactionFlow(counterpartySession, SignTransactionFlow.tracker()){
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                val output =stx.tx.outputs.single().data
+                "This must be a Alok transaction" using (output is AlokState)
+
+                val alok = output as AlokState
+                "The issuer of a Alok must be the issuing node" using (alok.issuer.owningKey == ourIdentity.owningKey)
+            }
+        }
+        subFlow(signTransactionFlow)
+    }
+}
+
+// ***************************************************
+// **************** Extra code ***********************
+// ***************************************************
+
+// ***********
+// * Plugins *
+// ***********
+class TemplateWebPlugin : WebServerPluginRegistry {
+    // A list of classes that expose web JAX-RS REST APIs.
+    override val webApis: List<Function<CordaRPCOps, out Any>> = listOf(Function(::TemplateApi))
+    //A list of directories in the resources directory that will be served by Jetty under /web.
+    // This template's web frontend is accessible at /web/template.
+    override val staticServeDirs: Map<String, String> = mapOf(
+            // This will serve the templateWeb directory in resources to /web/template
+            "template" to javaClass.classLoader.getResource("templateWeb").toExternalForm()
+    )
+}
+
+// Serialization whitelist.
+class TemplateSerializationWhitelist : SerializationWhitelist {
+    override val whitelist: List<Class<*>> = listOf(TemplateData::class.java)
+}
+
+// This class is not annotated with @CordaSerializable, so it must be added to the serialization whitelist, above, if
+// we want to send it to other nodes within a flow.
+data class TemplateData(val payload: String)
